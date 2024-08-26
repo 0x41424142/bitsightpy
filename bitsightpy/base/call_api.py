145cac0f1ebbd8bb627de2e002f6a70aa7c15989
0x4A616B65 <jake.lindsay@thermofisher.com>
2024-08-26 09:02:10 -0400

    Added CLI functionality

--- bitsightpy/base/call_api.py
+++ bitsightpy/base/call_api.py
@@ -190,9 +190,7 @@ def do_paginated_call(
             params=(
                 kwargs["params"].copy()
                 if "params" in kwargs
-                else kwargs.copy()
-                if kwargs
-                else None
+                else kwargs.copy() if kwargs else None
             ),
             headers=headers,
             override_method=override_method,
